17fb6bffc626b6d76a94103708ac30d30f78c47a
nurtaims <nurtaims@gmail.com>
2019-01-16 18:48:03 +0600

    scrollview

--- src/components/articles/TitleComponent.js
+++ src/components/articles/TitleComponent.js
@@ -1,12 +1,40 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet, PixelRatio } from 'react-native'
 
 export default class TitleComponent extends Component{
     render(){
         return(
-            <View>
-                <Text>TitleComponent</Text>
+            <View style={styles.container}>
+                <View style={styles.titleView}>
+                    <Text style={[styles.title,{fontSize:this.props.fontSize}]}>{this.props.title}</Text>
+                </View>
+                <View style={styles.dateView}>
+                    <Text style={styles.date}>Published at {this.props.publishedAt}</Text>
+                </View>
             </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        borderBottomWidth: 1,
+        borderBottomColor: '#C0C0C0',
+        paddingBottom: '4%'
+    },
+    titleView:{
+        marginTop: '2%'
+    },
+    title: {
+        fontFamily: 'georgia',
+        color: 'black'
+    },
+    date: {
+        fontFamily: 'georgia',
+        color: '#808080',
+        fontSize: PixelRatio.getPixelSizeForLayoutSize(5)
+    },
+    dateView:{
+        marginTop: '2%'
+    }
+})
--- src/features/news/components/NewsComponent.js
+++ src/features/news/components/NewsComponent.js
@@ -1,31 +1,41 @@
 import React, { Component } from 'react'
-import { StyleSheet, View, Text, PixelRatio, TextInput } from 'react-native'
-import { Constants } from 'expo'
-import { Button } from 'react-native-elements'
+import { StyleSheet, View, Text, PixelRatio, ScrollView, TouchableOpacity } from 'react-native'
 import TitleComponent from '../../../components/articles/TitleComponent'
-import Header from './HeaderComponent'
 
 export default class NewsComponent extends Component<{}>{
-    constructor(props){
-        super(props)
-        this.state = {
-            visibleSearchBar: false,
-            searchText: ''
-        }
-    }
     render(){
         return(
-            <View style={{backgroundColor:'#fff'}}>
-                <View style={{height:Constants.statusBarHeight, backgroundColor:'black'}}/>
-                <Header />
-                <View style={{alignItems:'center',width:'95%',marginLeft:'2.5%'}}>
-                    <TitleComponent />
-                </View>
-            </View>
+            <ScrollView style={{backgroundColor:'#fff'}}>
+                <TouchableOpacity
+                    style={styles.titleView}
+                    onPress={()=>console.log('News')}
+                >
+                    <View style={styles.sourceView}>
+                        <Text style={styles.source}>The Guardian (AU)</Text>
+                    </View>
+                    <TitleComponent 
+                        title='Spanish police arrest 182 people in raid on illegal cockfight'
+                        publishedAt='2019-01-14T18:02:07Z'
+                        fontSize={PixelRatio.getPixelSizeForLayoutSize(7)}
+                    />
+                </TouchableOpacity>
+            </ScrollView>
         )
     }
 }
 
 const styles = StyleSheet.create({
-    
+    titleView: {
+        width:'95%',
+        marginLeft:'2.5%',
+        marginTop: "2%",
+    },
+    source: {
+        fontFamily: 'georgia',
+        color: 'green',
+        fontSize: PixelRatio.getPixelSizeForLayoutSize(5)
+    },
+    sourceView:{
+        marginTop: '2%'
+    }
 })
