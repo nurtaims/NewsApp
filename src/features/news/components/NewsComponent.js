77ffaef7989233401ad69540bab78f8bb8f7fe89
nurtaims <nurtaims@gmail.com>
2019-01-16 18:12:20 +0600

    header_component

--- src/features/news/components/NewsComponent.js
+++ src/features/news/components/NewsComponent.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import { StyleSheet, View, Text, PixelRatio, TextInput } from 'react-native'
 import { Constants } from 'expo'
 import { Button } from 'react-native-elements'
+import TitleComponent from '../../../components/articles/TitleComponent'
+import Header from './HeaderComponent'
 
 export default class NewsComponent extends Component<{}>{
     constructor(props){
@@ -15,50 +17,9 @@ export default class NewsComponent extends Component<{}>{
         return(
             <View style={{backgroundColor:'#fff'}}>
                 <View style={{height:Constants.statusBarHeight, backgroundColor:'black'}}/>
-                {
-                    this.state.visibleSearchBar ? 
-                        <View style={styles.header}>
-                            <View style={styles.searchInputView}>
-                                <TextInput 
-                                    placeholder='Search'
-                                    style={styles.searchInput}
-                                    autoFocus={true}
-                                    onChangeText={(text)=>this.setState({searchText: text})}
-                                />
-                            </View>
-                            <View style={styles.cancelView}>
-                                <Button 
-                                    title='Cancel'
-                                    fontSize={PixelRatio.getPixelSizeForLayoutSize(5)}
-                                    borderRadius={PixelRatio.getPixelSizeForLayoutSize(11)}
-                                    backgroundColor='#fff'
-                                    color='black'
-                                    buttonStyle={styles.cancel_btn}
-                                    onPress={()=>this.setState({visibleSearchBar:false, searchText:''})}
-                                />
-                            </View>
-                        </View>
-                    :
-                    <View style={styles.header}>
-                        <View style={styles.headerTitle}>
-                            <Text style={styles.headerText}>NEWS</Text>
-                            <Text style={[styles.headerText,{color:'red',fontWeight:'500'}]}>APP</Text>
-                        </View>
-                        <View style={styles.search}>
-                            <Button
-                                title='Search'
-                                buttonStyle={styles.search_btn}
-                                borderRadius={PixelRatio.getPixelSizeForLayoutSize(10)}
-                                fontSize={PixelRatio.getPixelSizeForLayoutSize(5)}
-                                backgroundColor='#fff'
-                                color='black'
-                                onPress={()=>this.setState({visibleSearchBar:true})}
-                            />
-                        </View>
-                    </View>
-                }
+                <Header />
                 <View style={{alignItems:'center',width:'95%',marginLeft:'2.5%'}}>
-                    
+                    <TitleComponent />
                 </View>
             </View>
         )
@@ -66,46 +27,5 @@ export default class NewsComponent extends Component<{}>{
 }
 
 const styles = StyleSheet.create({
-    header: {
-        height: PixelRatio.getPixelSizeForLayoutSize(18),
-        width: '100%',
-        alignItems: 'center',
-        justifyContent: 'space-between',
-        borderBottomWidth: 1,
-        borderColor: '#C0C0C0',
-        flexDirection: 'row'
-    },
-    headerTitle: {
-        flexDirection: 'row',
-        marginLeft: PixelRatio.getPixelSizeForLayoutSize(55),
-    },
-    headerText:{
-        fontFamily:'happyMonkey',
-        fontSize: PixelRatio.getPixelSizeForLayoutSize(6)
-    },
-    search_btn: {
-        paddingVertical: PixelRatio.getPixelSizeForLayoutSize(1),
-        paddingHorizontal: PixelRatio.getPixelSizeForLayoutSize(8),
-        borderWidth: 1,
-        borderColor:'#C0C0C0'
-    },
-    searchInputView: {
-        width: '75%',
-        paddingLeft: PixelRatio.getPixelSizeForLayoutSize(3)
-    },
-    cancelView: {
-        width: '25%',
-    },
-    cancel_btn: {
-        borderWidth: 1,
-        borderColor: '#C0C0C0',
-        paddingVertical: PixelRatio.getPixelSizeForLayoutSize(1.5),
-        paddingHorizontal: PixelRatio.getPixelSizeForLayoutSize(4),
-    },
-    searchInput: {
-        borderWidth: 1,
-        borderColor: '#C0C0C0',
-        borderRadius: PixelRatio.getPixelSizeForLayoutSize(10),
-        paddingLeft: PixelRatio.getPixelSizeForLayoutSize(3)
-    }
+    
 })
